0837dc0ba2a8ccebf74eba9c7979c095ba083c1d
Ryan Endacott <rzeg24@gmail.com>
2014-10-15 22:45:51 -0700

    Save refresh token.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
+      user.refresh_token = auth['credentials']['refresh_token']
       if auth['info']
          user.name = auth['info']['name'] || ""
       end
