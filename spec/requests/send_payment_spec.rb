4a7b03ff3cb16f4c02a9c015cab0dccfcba1c0c1
Ryan Endacott <rzeg24@gmail.com>
2014-10-16 23:31:54 -0700

    Finished full suite of tests for send payment.

--- spec/requests/send_payment_spec.rb
+++ spec/requests/send_payment_spec.rb
@@ -13,7 +13,7 @@ describe "POST 'send_payment'", :omniauth do
   end
 
   it "calls with correct auth and returns 200 if no errors" do
-    allow(Dwolla::Transactions).to receive(:send).and_return('Success!')
+    allow(Dwolla::Transactions).to receive(:send).and_return('It worked.')
     expect(Dwolla::Transactions).to receive(:send).with({
       destinationId: 'hello@world.com',
       destinationType: 'Email',
@@ -23,6 +23,22 @@ describe "POST 'send_payment'", :omniauth do
     post '/send_payment', format: :json, uid: '60', api_token: '1',
         amount: 1, email: 'hello@world.com', pin: 1111
     expect(response.response_code).to eq(200)
+    expect(JSON.parse(response.body)).to eq({'message' => 'Success!'})
   end
+
+  it "calls with correct auth and returns 400 if there are errors" do
+    allow(Dwolla::Transactions).to receive(:send).and_raise('Invalid PIN.')
+    expect(Dwolla::Transactions).to receive(:send).with({
+      destinationId: 'hello@world.com',
+      destinationType: 'Email',
+      amount: '1',
+      pin: '1111'
+    })
+    post '/send_payment', format: :json, uid: '60', api_token: '1',
+        amount: 1, email: 'hello@world.com', pin: 1111
+    expect(response.response_code).to eq(400)
+    expect(JSON.parse(response.body)).to eq({'error' => 'Invalid PIN.'})
+  end
+
 
 end
